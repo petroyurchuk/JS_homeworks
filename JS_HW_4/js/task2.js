bd8ce74907e3e02e3874be0083661816f8d4b3dd
PetroYurchuk <petroyurchuk66@gmail.com>
2023-01-10 17:36:08 +0200

    Added fourth homework

---
+++ JS_HW_4/js/task2.js
@@ -0,0 +1,29 @@
+const inventory = {
+  items: ["Knife", "Gas mask"],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter((item) => item !== itemName);
+  },
+};
+
+const invokeInventoryAction = function (itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
+};
+
+invokeInventoryAction("Medkit", (itemName) => inventory.add(itemName));
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction("Gas mask", (itemName) => inventory.remove(itemName));
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
